f769c1046b9977da778b0f4305147dd11abf289d
Hazem Elsayed <hazem.elsayed@zyda.com>
2022-12-28 15:59:12 +0200

    add endpoint to calculate the quiz result

--- server/quiz/urls.py
+++ server/quiz/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
-from quiz.views import QuestionView
+from quiz.views import QuestionView, ResultsView
 
 router = routers.DefaultRouter()
 router.register(r'question', QuestionView)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('result', ResultsView.as_view({'post': 'result'}))
 ]
--- server/quiz/views.py
+++ server/quiz/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, exceptions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from quiz.models.questions import Question
 from quiz.serializers import QuestionSerilizer
@@ -8,3 +10,18 @@ from quiz.serializers import QuestionSerilizer
 class QuestionView(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerilizer
+    
+class ResultsView(viewsets.GenericViewSet):
+    @action(detail=False, methods=["post"])
+    def result(self, request):
+        answers = request.data
+        if not isinstance(answers, list) or any(not isinstance(i,int) for i in answers): 
+            raise exceptions.APIException("please submit an array of integars", 400)
+        number_of_questions = len(answers)
+        max_points = number_of_questions * 100
+        points = 0
+        for answer in answers:
+            points += answer * 25
+        if points <= max_points / 2:
+            return Response({ "result": "Introvert" })
+        return Response({ "result": "Extrovert" })
