df9b927a058471bfe37b2281836d5d6f1039b2df
Hazem Elsayed <hazem.elsayed@zyda.com>
2022-12-28 16:00:48 +0200

    add result page

---
+++ frontend/src/app/result-page/result-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-result-page',
+  templateUrl: './result-page.component.html',
+  styleUrls: ['./result-page.component.css']
+})
+export class ResultPageComponent implements OnInit {
+  state: any
+  constructor() { }
+
+  ngOnInit(): void {
+    this.state = history.state;
+  }
+
+}
